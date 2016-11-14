ff00cfd2c66db8aa395ddd27fc115538bb6db1b4
pulkit <pulkit.saxena93@gmail.com>
2016-11-15 00:13:09 +0530

    Initial commit

---
+++ conf/serverConf.js
@@ -0,0 +1,13 @@
+// app config variable
+
+var CONF = {
+
+  // URL to crawl upon
+  URL                       : 'https://medium.com/',
+
+  // number of concurrent requests allowed to URL
+  MAX_CONCURRENT_REQUESTS   : 5,
+
+};
+
+module.exports = CONF;
